488f201740d499ada649d52a3ff140cb8e7d73b3
devmontini <devmontini1@gmail.com>
2022-01-18 20:19:12 -0300

    LISTO

--- api/tests/routes/activity.spec.js
+++ api/tests/routes/activity.spec.js
@@ -20,25 +20,24 @@ describe('Activity routes', () => {
   }));
   beforeEach(() => Activity.sync({ force: true })
     .then(() => Activity.create(activiti))); 
+});
 
-    // ME MOSTRARA LAS ACTIVIDADES
-    describe('POST /activities', () => {
-        it('should get 200', () =>{
-            agent.post('/activities')
-            .send({
-                id: 1,
-                name: 'asdasd',
-                dificultad: 3,
-                duracion: '1.40',
-                temporada: "Verano",
-            }).expect(200);
-        });
+// ME MOSTRARA LAS ACTIVIDADES
+  describe('POST /activities', () => {
+    it('should get 200', () =>{
+        agent.post('/activities')
+        .send({
+            id: 1,
+            name: 'asdasd',
+            dificultad: 3,
+            duracion: '1.40',
+            temporada: "Verano",
+        }).expect(200);
     });
+});
 
-    describe('GET /activities', () => {
-        it('should get 200', () =>
-            agent.get('/activities').expect(200)
-        );
-    });
-
+describe('GET /activities', () => {
+    it('should get 200', () =>
+        agent.get('/activities').expect(200)
+    );
 });
--- client/src/test/landing.test.js
+++
@@ -1,9 +0,0 @@
-// import LandingPage from "../components/landing";
-
-
-// describe('asda', () => {
-//   it('should get 200',async () =>{
-//     const data = await LandingPage(true);
-//     expect(data.find({name: "landing" }));
-//   })
-// })
