205025f9fa620edf80141c88ae261532745416f0
devmontini <devmontini1@gmail.com>
2022-01-18 22:53:51 -0300

    pruebas

--- api/src/models/Activity.js
+++ api/src/models/Activity.js
@@ -5,6 +5,12 @@ module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('activity', {
 
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
